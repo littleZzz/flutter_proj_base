6cbe5f9b59b4c1a091e8d8a0aa62abc090e9a32f
wz <>
2024-10-14 11:23:17 +0800

    fixcommit

--- lib/net/env.dart
+++ lib/net/env.dart
@@ -4,27 +4,19 @@ import 'dart:io';
 
 class Environment {
   static bool production = false;
-  static String get apiEntry =>
-      production ? 'https://api.hoobuy.com' : 'https://dev.api.hoobuy.com';
+  static String get apiEntry => production ? '' : 'https://dev.api.hoobuy.com';
   static String get webEntry =>
-      production ? 'https://hoobuy.com' : 'https://dev-d8okw12xt.hoobuy.com';
+      production ? '' : 'https://dev-d8okw12xt.hoobuy.com';
 
   /// 图片上传接口
-  static String get uploadLocalMediaEntry => production
-      ? 'https://api.scm.hoobuy.com/scm'
-      : 'https://devapi.scm.hoobuy.com/scm';
+  static String get uploadLocalMediaEntry =>
+      production ? 'h' : 'https://devapi.scm.hoobuy.com/scm';
 
   static String get signKey => production
-      ? (Platform.isIOS
-          ? 'AA8F16D1-686D-4190-9B66-C97927AC1186'
-          : '59AE34F6-5FBD-49D6-9323-938C3DF8DDCE')
-      : (Platform.isIOS
-          ? '48056E6F-4B22-4BE6-9BBB-C24CB4574ED6'
-          : 'D0CA4A82-8461-4DE5-8399-54D06BB3347C');
+      ? (Platform.isIOS ? 'A86' : '59AE3E')
+      : (Platform.isIOS ? '480574ED6' : 'D0CA4ABB3347C');
 
   static String get signVersion => production
-      ? (Platform.isIOS
-          ? 'hoobuy-production-ios-1.0'
-          : 'hoobuy-production-android-1.0')
-      : (Platform.isIOS ? 'hoobuy-dev-ios-1.0' : 'hoobuy-dev-android-1.0');
+      ? (Platform.isIOS ? 'hoob' : 'hoob')
+      : (Platform.isIOS ? 'hoo' : 'hoob');
 }
